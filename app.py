f6fc17f629063f6d10e90204c5c1b179399deece
wavi23 <wiktoreq12@op.pl>
2025-02-23 21:43:59 +0100

    .

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+posts = [
+	{
+		'author': "Pedalinio",
+		'title': "Tytuł posta 1",
+		'content': "Opis posta 1",
+		'date_posted': "22 Luty, 2025"
+	},
+	{
+		'author': "Pedalinio",
+		'title': "Tytył posta 2",
+		'content': "Opis posta 2",
+		'date_posted': "23 Luty, 2025"
+	}
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+	return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+	return render_template('about.html', title="About")
+
+if __name__ == '__main__':
+	app.run(debug=True)
