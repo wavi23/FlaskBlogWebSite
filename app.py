d352f2ed8dc70e762b316f502cbabc5df76b89ec
wavi23 <wiktoreq12@op.pl>
2025-02-24 13:32:50 +0100

    .

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, url_for
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 't5d4D93si3m3tLMeyv1W171nOvQ3FO6Mt4g3jkQsm6mdlR2PWZL43WO1VvCGo2G2N8VG0nLPK72'
 
 posts = [
 	{
@@ -25,6 +28,16 @@ def home():
 @app.route('/about')
 def about():
 	return render_template('about.html', title="About")
+
+@app.route('/register')
+def register():
+	form = RegistrationForm()
+	return render_template('register.html', title="Rejestracja", form=form)
+
+@app.route('/login')
+def login():
+	form = LoginForm()
+	return render_template('login.html', title="Logowanie", form=form)
 
 if __name__ == '__main__':
 	app.run(debug=True)
